bffc36ad02cdfa0c2e27114ee6158c582d81257c
Alexandra <  >
2024-01-05 06:16:05 +0300

    Add site blocks with play

--- js/hw-2.5.js
+++ js/hw-2.5.js
@@ -1,111 +1,138 @@
-// Задание 1
-function getSmallerNumber() {
+// // Задание 1
+// function getSmallerNumber() {
 
-     const num1 = parseInt(prompt('Введите первое число 1:'), 10);
-     const num2 = parseInt(prompt('Введите второе число:'), 10);
+//      const num1 = parseInt(prompt('Введите первое число 1:'), 10);
+//      const num2 = parseInt(prompt('Введите второе число:'), 10);
 
-     return num1 < num2 ? num1 : num2;
-}
-console.log(getSmallerNumber());
+//      return num1 < num2 ? num1 : num2;
+// }
+// console.log(getSmallerNumber());
 
-// Задание 2
+// // Задание 2
 
-function parityOfNumber() {
-     let num = parseInt(prompt('Введите число:'), 10);
+// function parityOfNumber() {
+//      let num = parseInt(prompt('Введите число:'), 10);
 
-     return num % 2 === 0 ? 'Число четное' : 'Число нечетное';
-}
+//      return num % 2 === 0 ? 'Число четное' : 'Число нечетное';
+// }
 
-parityOfNumber();
+// parityOfNumber();
 
-// Задание 3
+// // Задание 3
 
-function getSquare() {
-     const num = parseInt(prompt('Введите число:'), 10);
-     const result = num * num;
-     console.log(result);
-}
-getSquare();
+// function getSquare() {
+//      const num = parseInt(prompt('Введите число:'), 10);
+//      const result = num * num;
+//      console.log(result);
+// }
+// getSquare();
 
-function squareNumber(number) {
-     return number * number;
-}
+// function squareNumber(number) {
+//      return number * number;
+// }
 
-// Задание 4
+// // Задание 4
 
-function getUserAge() {
-     let age = prompt('Сколько вам лет?');
+// function getUserAge() {
+//      let age = prompt('Сколько вам лет?');
 
-     if (age < 0 || age !== Number(age) || age === '') {
-          console.log('Вы ввели неправильное значение');
-     } else if (age >= 0 && age <= 12) {
-          console.log('Привет, друг!');
-     } else {
-          console.log('Добро пожаловать!');
-     }
-}
+//      if (age < 0 || age !== Number(age) || age === '') {
+//           console.log('Вы ввели неправильное значение');
+//      } else if (age >= 0 && age <= 12) {
+//           console.log('Привет, друг!');
+//      } else {
+//           console.log('Добро пожаловать!');
+//      }
+// }
 
-getUserAge();
+// getUserAge();
 
-// Задание 5
+// // Задание 5
 
-function checkIsNumber() {
-     let num1 = prompt('Введите первое число:');
-     let num2 = prompt('Введите второе число:');
+// function checkIsNumber() {
+//      let num1 = prompt('Введите первое число:');
+//      let num2 = prompt('Введите второе число:');
 
-     if (isNaN(num1) || isNaN(num2)) {
-          return 'Одно или оба значения не являются числом';
-     } else {
-          return num1 * num2;
-     }
-}
+//      if (isNaN(num1) || isNaN(num2)) {
+//           return 'Одно или оба значения не являются числом';
+//      } else {
+//           return num1 * num2;
+//      }
+// }
 
-checkIsNumber();
+// checkIsNumber();
 
-// Задание 6
+// // Задание 6
 
-function cubeNumber() {
-     let number = prompt('Введите число');
+// function cubeNumber() {
+//      let number = prompt('Введите число');
 
-     if (isNaN(number)) {
-          return 'Переданный параметр не является числом';
-     } else {
-          const cube = number ** 3;
-          return `${number} в кубе равняется ${cube}`;
-     }
-}
+//      if (isNaN(number)) {
+//           return 'Переданный параметр не является числом';
+//      } else {
+//           const cube = number ** 3;
+//           return `${number} в кубе равняется ${cube}`;
+//      }
+// }
 
-// Проверяем функцию с параметрами от 0 до 10 включительно
-for (let i = 0; i <= 10; i++) {
-     console.log(cubeNumber(i));
-}
+// // Проверяем функцию с параметрами от 0 до 10 включительно
+// for (let i = 0; i <= 10; i++) {
+//      console.log(cubeNumber(i));
+// }
 
-// Задание 7
+// // Задание 7
 
-const circle1 = {
-     radius: 10,
-     getArea: function () {
-          return this.radius * this.radius;
-     }
-};
+// const circle1 = {
+//      radius: 10,
+//      getArea: function () {
+//           return this.radius * this.radius;
+//      }
+// };
 
-console.log(circle1.getArea());
+// console.log(circle1.getArea());
 
-const circle2 = {
-     radius: 12,
-     getPerimeter: function () {
-          return 2 * this.radius;
-     }
-}
+// const circle2 = {
+//      radius: 12,
+//      getPerimeter: function () {
+//           return 2 * this.radius;
+//      }
+// }
 
-console.log(circle2.getPerimeter());
+// console.log(circle2.getPerimeter());
 
 // Задание 8
 
+// function getSeasonByMonth() {
+//      let monthNumber = prompt('Введите номер месяца');
+
+//      if (monthNumber < 1 || monthNumber > 12 || isNaN(monthNumber)) {
+//           return 'Некорректный параметр: введите число от 1 до 12';
+//      } else if (monthNumber === 12 || monthNumber === 1 || monthNumber === 2) {
+//           return 'Зима';
+//      } else if (monthNumber >= 3 && monthNumber <= 5) {
+//           return 'Весна';
+//      } else if (monthNumber >= 6 && monthNumber <= 8) {
+//           return 'Лето';
+//      } else if (monthNumber >= 9 && monthNumber <= 11) {
+//           return 'Осень';
+//      }
+
+// }
+
+// getSeasonByMonth();
+document.addEventListener('DOMContentLoaded', function () {
+     var playButton = document.getElementById('playButton');
+
+     playButton.addEventListener('click', function () {
+          alert(getSeasonByMonth());
+     });
+});
+
 function getSeasonByMonth() {
      let monthNumber = prompt('Введите номер месяца');
+     monthNumber = parseInt(monthNumber);
 
-     if (monthNumber < 1 || monthNumber > 12 || isNaN(monthNumber)) {
+     if (isNaN(monthNumber) || monthNumber < 1 || monthNumber > 12) {
           return 'Некорректный параметр: введите число от 1 до 12';
      } else if (monthNumber === 12 || monthNumber === 1 || monthNumber === 2) {
           return 'Зима';
@@ -119,4 +146,5 @@ function getSeasonByMonth() {
 
 }
 
-getSeasonByMonth();
+
+
